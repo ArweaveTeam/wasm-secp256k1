a8c146eaa9b5583658e31384be44784ee69b58c4
Ros McMahon <rosmcmahon@gmail.com>
2025-02-21 10:24:44 +0700

    feat: browser support with base64ToBuffer

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-export type {SignatureAndRecovery, RecoveryValue } from './api/secp256k1-types';
+export type { SignatureAndRecovery, RecoveryValue } from './api/secp256k1-types';
 export { ByteLens } from './api/secp256k1-types';
 export type { Secp256k1 } from './api/secp256k1';
 
 import { Secp256k1, WasmSecp256k1 } from './api/secp256k1';
 import SB64_SECP256K1_WASM from './gen/secp256k1.wasm';
 
-export const initWasmSecp256k1 = (): Promise<Secp256k1> => WasmSecp256k1(Buffer.from(SB64_SECP256K1_WASM, 'base64'));
+const base64ToBuffer = (b64: string) => typeof window !== undefined
+	? Uint8Array.from(atob(b64), c => c.charCodeAt(0))
+	: Buffer.from(b64, 'base64');
+
+export const initWasmSecp256k1 = (): Promise<Secp256k1> => WasmSecp256k1(base64ToBuffer(SB64_SECP256K1_WASM));
